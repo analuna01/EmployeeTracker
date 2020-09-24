f2ad5d975787dca1e9a22cd1049c8e7714c308ab
Ana Luna <luna.ana92@gmail.com>
2020-09-23 23:59:10 -0700

    added a connection and a new lib folder

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    database: "(..db/schema.sql)",
+    host: 8080,
+    database: employeeTracker_db,
     user: "root",
     password: "root",
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.error('Error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('Connected as id ' + connection.threadId);
 });
